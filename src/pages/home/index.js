ad17e4c5a65847c4401693396e3d78c35a68f5eb
朱彦霖 <zhuyanlin@renrenche.com>
2019-02-13 18:53:55 +0800

    router

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>home</p>
+      </Fragment>
+    )
+  }
+}
+
+export default Home
