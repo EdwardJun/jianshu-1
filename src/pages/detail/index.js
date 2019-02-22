1a852318c2a9db101dfb26f55fc10edd0bf74083
朱彦霖 <zhuyanlin@renrenche.com>
2019-02-22 16:03:24 +0800

    js文件按需加载

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,13 +1,16 @@
 import React, { PureComponent, Fragment } from 'react'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Detail extends PureComponent {
   render() {
     return (
       <Fragment>
         <p>detail</p>
+        {this.props.match.params.id}
       </Fragment>
     )
   }
 }
 
-export default Detail
+export default connect(null, null)(withRouter(Detail))
